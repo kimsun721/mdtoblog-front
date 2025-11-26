f387e6970ac23095d89377199ef2bd18d662541b
kimsun721 <kimsun050820@gmail.com>
2025-11-26 20:30:38 +0900

    refresh token 로직 layout.ts -> layout.svelte로 이동

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import Header from '../lib/Header.svelte';
 	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/Header.svelte';
+
+	import { onMount } from 'svelte';
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import { accessToken } from '$lib/auth';
+
 	export let data: { accessToken: string | null };
 
+	// 초기 서버 데이터 반영
 	$: if (data.accessToken) {
 		accessToken.set(data.accessToken);
 	}
+
+	// 클라이언트에서 refresh
+	onMount(async () => {
+		try {
+			const res = await fetch(`${PUBLIC_API_URL}/auth/refresh`, {
+				method: 'POST',
+				credentials: 'include'
+			});
+
+			const json = await res.json();
+			if (json?.data) {
+				accessToken.set(json.data);
+			}
+		} catch (e) {
+			console.error('refresh fail', e);
+		}
+	});
 </script>
 
 <Header {data} />
